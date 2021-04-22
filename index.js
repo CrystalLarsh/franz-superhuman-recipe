2b8b3fe4786ea12c71f5593920c104408b8d1b72
CrystalLarsh <44876824+CrystalLarsh@users.noreply.github.com>
2021-04-21 22:52:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-// updated class name to superhuman
-module.exports = Franz => class superhuman extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
+// just pass through Franz - Superhuman tab appears, says needs Chrome
+// module.exports = Franz => Franz;
 
-// try the whole string from the issue example? class name kept it from showing up as selectable
+// tried the whole string from the issue example - Superhuman tab appears, says needs Chrome
 // module.exports = Franz => class useragent extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
 
-// tried shortening to just Chrome lol - didn't work
-// module.exports = Franz => class superhuman extends Franz { overrideUserAgent() {return "Chrome"; } };
+// updated class name to superhuman - Superhuman tab appears, says needs Chrome
+module.exports = Franz => class superhuman extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
 
-// just pass through Franz - doesn't work
-//module.exports = Franz => Franz;
+// Tried shortening to just Chrome lol - Superhuman tab appears, says needs Chrome
+// module.exports = Franz => class superhuman extends Franz { overrideUserAgent() {return "Chrome"; } };
