70000e8f6e991b9c44271ce2a038023020499359
CrystalLarsh <44876824+CrystalLarsh@users.noreply.github.com>
2021-04-21 22:37:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-// use chrome to load
-module.exports = Franz => class superhuman extends Franz { overrideUserAgent() {return "Chrome"; } };
+// updated class name to superhuman
+module.exports = Franz => class superhuman extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
 
-// just pass through Franz
+// try the whole string from the issue example? class name kept it from showing up as selectable
+// module.exports = Franz => class useragent extends Franz { overrideUserAgent() { return "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.10136"; } };
+
+// tried shortening to just Chrome lol - didn't work
+// module.exports = Franz => class superhuman extends Franz { overrideUserAgent() {return "Chrome"; } };
+
+// just pass through Franz - doesn't work
 //module.exports = Franz => Franz;
